51900ba269f753ab25800cb467daae0c7b59c2fb
shrey3219 <shreyjain209@gmail.com>
2023-07-18 18:50:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+
+/*const heading = React.createElement("h1",{},"SHREY JAIN");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+*/
+
+
+/*let x=0;
+if(x==0){
+    alert("ufeufheru");
+
+    
+}
+else{
+  alert("bvebf")
+}*/
+
+
+
+/*const paret = React.createElement(
+    "div",
+    { id:"parent"},
+    React.createElement(
+        "div",
+        { id:"child"},
+        React.createElement("h1",{},"FDzK"  )
+    )
+);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(paret);
+*/
+
+
+
+//create an array to do that but this looks so ugly so we use jsx
+/*const paret = React.createElement(
+    "div",
+    { id:"parent"},
+    [React.createElement(
+        "div",
+        { id:"child"},
+       [ React.createElement("h1",{},"FDzK"),  React.createElement("h2",{},"shrey jain") ]
+    ), React.createElement(
+        "div",
+        { id:"child2"},
+       [ React.createElement("h1",{},"FDzK"),  React.createElement("h2",{},"shrey jain") ]
+    )]
+);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(paret);
+*/
+
