696386a86ea546f83790e9fa76d484f405249aa3
shrey3219 <shreyjain209@gmail.com>
2023-07-19 19:34:40 +0530

    first commit

--- App.js
+++ App.js
@@ -1,67 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 
+import React from "react";
+import  ReactDOM from "react-dom";
 
-
-
-
-/*const heading = React.createElement("h1",{},"SHREY JAIN");
+const heading = React.createElement("h1",{},"SHREY JAIN");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-*/
-
-
-/*let x=0;
-if(x==0){
-    alert("ufeufheru");
-
-    
-}
-else{
-  alert("bvebf")
-}*/
-
-
-
-/*const paret = React.createElement(
-    "div",
-    { id:"parent"},
-    React.createElement(
-        "div",
-        { id:"child"},
-        React.createElement("h1",{},"FDzK"  )
-    )
-);
-
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(paret);
-*/
-
-
-
-//create an array to do that but this looks so ugly so we use jsx
-/*const paret = React.createElement(
-    "div",
-    { id:"parent"},
-    [React.createElement(
-        "div",
-        { id:"child"},
-       [ React.createElement("h1",{},"FDzK"),  React.createElement("h2",{},"shrey jain") ]
-    ), React.createElement(
-        "div",
-        { id:"child2"},
-       [ React.createElement("h1",{},"FDzK"),  React.createElement("h2",{},"shrey jain") ]
-    )]
-);
-
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(paret);
-*/
-
-
-
